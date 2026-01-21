d6c94d16ad0af827cb87f8822f6e4ca586007d93
Daniel Canoy <canoydaniel06@gmail.com>
2026-01-21 22:45:26 +0800

    recommit

--- lib/data/job-board.ts
+++ lib/data/job-board.ts
@@ -1,7 +1,9 @@
 import { prisma } from "@/prisma/prisma";
 import { unstable_cache } from "next/cache";
+import { Job } from "@prisma/client";
 
 export const getAvailableJobs = unstable_cache(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async (companySlug: string, userId?: string) => {
     const company = await prisma.company.findFirst({
       where: { slug: companySlug },
@@ -18,7 +20,7 @@ export const getAvailableJobs = unstable_cache(
       },
     });
 
-    return availableJobs.map((job) => ({
+    return availableJobs.map((job: Job & { SavedJob: { id: string }[] }) => ({
       ...job,
       isSaved: job.SavedJob.length > 0,
     }));
